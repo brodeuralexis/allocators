4f1bc2ec0d6de5adcb007fcba4de72a016b2328d
Alexis Brodeur <brodeuralexis@gmail.com>
2020-03-03 20:13:01 -0500

    Heavier direct_allocator test

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 #define ASTR_LEN 3
 const char* STR = "Hello World!";
 
-#define ARRAY_SIZE (16 * 1024)
+#define ARRAY_SIZE (4)
+#define SUBARRAY_SIZE (1024)
 
 int main()
 {
     allocator_t* allocator = direct_allocator;
 
-    double* array = create_array(allocator, double, ARRAY_SIZE);
+    double** array = create_array(allocator, double*, ARRAY_SIZE);
+
     for (size_t i = 0; i < ARRAY_SIZE; ++i)
     {
-        array[i] = (double) i;
+        array[i] = create_array(allocator, double, SUBARRAY_SIZE);
+    }
+
+    for (size_t i = 0; i < ARRAY_SIZE; ++i)
+    {
+        destroy_array(allocator, array[i]);
     }
 
     destroy_array(allocator, array);
