b645770cf1e3a8d5846e0e7c2f134785bd6b3306
Alexis Brodeur <brodeuralexis@gmail.com>
2020-03-10 21:42:20 -0400

    Make nodes public

--- include/allocators/fixed_buffer_allocator.h
+++ include/allocators/fixed_buffer_allocator.h
@@ -19,8 +19,87 @@ typedef struct {
 
 fixed_buffer_allocator_t fixed_buffer_allocator_init(fixed_buffer_strategy_t* strategy, void* buffer, size_t size);
 
+/**
+ * A node in the allocation list of a fixed buffer allocator.
+ */
+typedef struct fixed_buffer_node {
+    /**
+     * Indicates if this node is a hole.
+     */
+    bool is_hole;
+    /**
+     * The size of the memory following this node.
+     */
+    size_t size;
+    /**
+     * A pointer to the previous node in memory.
+     */
+    struct fixed_buffer_node* previous;
+} fixed_buffer_node_t;
+
+/**
+ * Returns a pointer to the memory associated with this node.
+ * @param node A node
+ * @return A pointer
+ */
+static inline void* fixed_buffer_node_memory(fixed_buffer_node_t* node)
+{
+    return (void*) (node + 1);
+}
+
+/**
+ * Returns a pointer to the end of this node and its associated memory.
+ * @param node A node
+ * @return A pointer
+ */
+static inline void* fixed_buffer_node_end(fixed_buffer_node_t* node)
+{
+    return (void*) ((char*) (node + 1) + node->size);
+}
+
+/**
+ * Returns the first node from an allocator
+ * @param allocator A fixed buffer allocator
+ * @return A node
+ */
+static inline fixed_buffer_node_t* fixed_buffer_node_first(fixed_buffer_allocator_t* allocator)
+{
+    return ((fixed_buffer_node_t*) allocator->buffer);
+}
+
+/**
+ * Returns the node following another node in the list of nodes associated with an allocator.
+ *
+ * If the following node would be out of the allocator's buffer, `NULL` is returned.
+ * @param allocator A fixed buffer allocator
+ * @param node A node
+ * @return The following node
+ */
+fixed_buffer_node_t* fixed_buffer_node_next(fixed_buffer_allocator_t* allocator, fixed_buffer_node_t* node);
+
+/**
+ * Finds a node for the given memory location.
+ *
+ * If the memory is not managed by this allocator, `NULL` is returned.
+ * @param allocator A fixed buffer allocator
+ * @param memory A pointer
+ * @return A node
+ */
+fixed_buffer_node_t* fixed_buffer_node_find(fixed_buffer_allocator_t* allocator, void* memory);
+
+/**
+ * Prints debug information associated with a fixed buffer allocator.
+ * @param allocator An allocator
+ * @param file A file handle
+ */
 void fixed_buffer_allocator_debug(fixed_buffer_allocator_t* allocator, FILE* file);
 
+/**
+ * Returns the memory offset of the given pointer.
+ * @param allocator A fixed buffer allocator
+ * @param memory A pointer
+ * @return An offset
+ */
 size_t fixed_buffer_allocator_debug_memory(fixed_buffer_allocator_t* allocator, void* memory);
 
 #endif
--- src/fixed_buffer_allocator.c
+++ src/fixed_buffer_allocator.c
@@ -5,68 +5,12 @@
 
 #include "./macros.h"
 
-/**
- * A node in the allocation list of a fixed buffer allocator.
- */
-typedef struct node {
-    /**
-     * Indicates if this node is a hole.
-     */
-    bool is_hole;
-    /**
-     * The size of the memory following this node.
-     */
-    size_t size;
-    /**
-     * A pointer to the previous node in memory.
-     */
-    struct node* previous;
-} node_t;
-
-/**
- * Returns a pointer to the memory associated with this node.
- * @param node A node
- * @return A pointer
- */
-static inline void* node_memory(node_t* node)
+fixed_buffer_node_t* fixed_buffer_node_next(fixed_buffer_allocator_t* allocator, fixed_buffer_node_t* node)
 {
-    return (void*) (node + 1);
-}
-
-/**
- * Returns a pointer to the end of this node and its associated memory.
- * @param node A node
- * @return A pointer
- */
-static void* node_end(node_t* node)
-{
-    return (void*) ((char*) (node + 1) + node->size);
-}
-
-/**
- * Returns the first node from an allocator
- * @param allocator A fixed buffer allocator
- * @return A node
- */
-static inline node_t* node_first(fixed_buffer_allocator_t* allocator)
-{
-    return ((node_t*) allocator->buffer);
-}
-
-/**
- * Returns the node following another node in the list of nodes associated with an allocator.
- *
- * If the following node would be out of the allocator's buffer, `NULL` is returned.
- * @param allocator A fixed buffer allocator
- * @param node A node
- * @return The following node
- */
-static node_t* node_next(fixed_buffer_allocator_t* allocator, node_t* node)
-{
-    node_t* next = (node_t*) node_end(node);
+    fixed_buffer_node_t* next = (fixed_buffer_node_t*) fixed_buffer_node_end(node);
 
     // Ensures that the next node is still in the buffer owned by our allocator.
-    if (((void*) next) < allocator->buffer || ((char*) next) >= ((char*) allocator->buffer) + allocator->size - sizeof(node_t))
+    if (((void*) next) < allocator->buffer || ((char*) next) >= ((char*) allocator->buffer) + allocator->size - sizeof(fixed_buffer_node_t))
     {
         return NULL;
     }
@@ -74,28 +18,20 @@ static node_t* node_next(fixed_buffer_allocator_t* allocator, node_t* node)
     return next;
 }
 
-/**
- * Finds a node for the given memory location.
- *
- * If the memory is not managed by this allocator, `NULL` is returned.
- * @param allocator A fixed buffer allocator
- * @param memory A pointer
- * @return A node
- */
-static node_t* node_find(fixed_buffer_allocator_t* allocator, void* memory)
+fixed_buffer_node_t* fixed_buffer_node_find(fixed_buffer_allocator_t* allocator, void* memory)
 {
-    node_t* node = node_first(allocator);
+    fixed_buffer_node_t* node = fixed_buffer_node_first(allocator);
 
     while (node != NULL)
     {
         // TODO(Alexis Brodeur): Consider allowing one to find a node when the memory is a pointer in the memory owned
         // by a node, and not only the start of the node's memory.
-        if (memory == node_memory(node))
+        if (memory == fixed_buffer_node_memory(node))
         {
             break;
         }
 
-        node = node_next(allocator,  node);
+        node = fixed_buffer_node_next(allocator,  node);
     }
 
     return node;
@@ -108,14 +44,14 @@ static node_t* node_find(fixed_buffer_allocator_t* allocator, void* memory)
  * @param size A size
  * @return The memory owned by the reserved node.
  */
-static void* node_reserve(fixed_buffer_allocator_t* allocator, node_t* node, size_t size)
+static void* node_reserve(fixed_buffer_allocator_t* allocator, fixed_buffer_node_t* node, size_t size)
 {
     UNUSED(allocator);
 
     assert(node != NULL);
     assert(node->is_hole);
 
-    size_t remaining_size = node->size - size - sizeof(node_t);
+    size_t remaining_size = node->size - size - sizeof(fixed_buffer_node_t);
 
     // If we were to create a new node after this one, do we have enough bytes left.
     // If not, this node does not change size.
@@ -123,12 +59,12 @@ static void* node_reserve(fixed_buffer_allocator_t* allocator, node_t* node, siz
     {
         node->size = size;
 
-        node_t* split = (node_t*) node_end(node);
+        fixed_buffer_node_t* split = (fixed_buffer_node_t*) fixed_buffer_node_end(node);
         split->is_hole = true;
         split->size = remaining_size;
         split->previous = node;
 
-        node_t* next = node_next(allocator, node);
+        fixed_buffer_node_t* next = fixed_buffer_node_next(allocator, node);
         if (next != NULL)
         {
             next->previous = split;
@@ -137,7 +73,7 @@ static void* node_reserve(fixed_buffer_allocator_t* allocator, node_t* node, siz
 
     node->is_hole = false;
 
-    return node_memory(node);
+    return fixed_buffer_node_memory(node);
 }
 
 /**
@@ -145,25 +81,25 @@ static void* node_reserve(fixed_buffer_allocator_t* allocator, node_t* node, siz
  * @param allocator A fixed buffer allocator
  * @param node A node to release
  */
-static void node_release(fixed_buffer_allocator_t* allocator, node_t* node)
+static void node_release(fixed_buffer_allocator_t* allocator, fixed_buffer_node_t* node)
 {
     assert(node != NULL);
     assert(!node->is_hole);
 
-    node_t* previous = node->previous;
-    node_t* next = node_next(allocator, node);
+    fixed_buffer_node_t* previous = node->previous;
+    fixed_buffer_node_t* next = fixed_buffer_node_next(allocator, node);
 
     // The previous node is a hole, make `node` a part of `previous` node.
     if (previous && previous->is_hole)
     {
-        previous->size += node->size + sizeof(node_t);
+        previous->size += node->size + sizeof(fixed_buffer_node_t);
         node = previous;
     }
 
     // The next node is a hole, make it a part of `node`.
     if (next && next->is_hole)
     {
-        node_t* after_next = node_next(allocator, next);
+        fixed_buffer_node_t* after_next = fixed_buffer_node_next(allocator, next);
 
         // Update backward references.
         if (after_next != NULL)
@@ -171,7 +107,7 @@ static void node_release(fixed_buffer_allocator_t* allocator, node_t* node)
             after_next->previous = node;
         }
 
-        node->size += next->size + sizeof(node_t);
+        node->size += next->size + sizeof(fixed_buffer_node_t);
     }
 
     // Override the tag to ensure that we have a hole in the buffer.
@@ -192,7 +128,7 @@ static void* first_fit_reallocate(allocator_t* _allocator, void* memory, size_t 
     }
     else if (memory == NULL)
     {
-        node_t* node = node_first(allocator);
+        fixed_buffer_node_t* node = fixed_buffer_node_first(allocator);
 
         while (node)
         {
@@ -202,7 +138,7 @@ static void* first_fit_reallocate(allocator_t* _allocator, void* memory, size_t 
                 break;
             }
 
-            node = node_next(allocator, node);
+            node = fixed_buffer_node_next(allocator, node);
         }
 
         // `node_next` returns NULL when traversal of the list finishes.
@@ -217,7 +153,7 @@ static void* first_fit_reallocate(allocator_t* _allocator, void* memory, size_t 
     }
     else if (size == 0)
     {
-        node_t* node = node_find(allocator, memory);
+        fixed_buffer_node_t* node = fixed_buffer_node_find(allocator, memory);
 
         if (node == NULL)
         {
@@ -230,7 +166,7 @@ static void* first_fit_reallocate(allocator_t* _allocator, void* memory, size_t 
     }
     else
     {
-        node_t* node = node_find(allocator, memory);
+        fixed_buffer_node_t* node = fixed_buffer_node_find(allocator, memory);
 
         if (node == NULL)
         {
@@ -266,9 +202,9 @@ fixed_buffer_allocator_t fixed_buffer_allocator_init(fixed_buffer_strategy_t* st
     allocator.buffer = buffer;
     allocator.size = size;
 
-    node_t* node = (node_t*) buffer;
+    fixed_buffer_node_t* node = (fixed_buffer_node_t*) buffer;
     node->is_hole = true;
-    node->size = size - sizeof(node_t);
+    node->size = size - sizeof(fixed_buffer_node_t);
     node->previous = NULL;
 
     return allocator;
@@ -276,20 +212,20 @@ fixed_buffer_allocator_t fixed_buffer_allocator_init(fixed_buffer_strategy_t* st
 
 void fixed_buffer_allocator_debug(fixed_buffer_allocator_t* allocator, FILE* file)
 {
-    node_t* node = node_first(allocator);
+    fixed_buffer_node_t* node = fixed_buffer_node_first(allocator);
 
-    fprintf(file, "[ (sizeof(node) == %zu)\n", sizeof(node_t));
+    fprintf(file, "[ (sizeof(node) == %zu)\n", sizeof(fixed_buffer_node_t));
 
     while (node != NULL)
     {
         fprintf(file, "\t%s @ %zu {\n", node->is_hole ? "hole" : "used", (size_t) (((char*) node) - ((char*) allocator->buffer)));
 
         fprintf(file, "\t\tsize   = %zu\n", node->size);
-        fprintf(file, "\t\tmemory = @ %zu\n", (size_t) (((char*) node_memory(node)) - ((char*) allocator->buffer)));
+        fprintf(file, "\t\tmemory = @ %zu\n", (size_t) (((char*) fixed_buffer_node_memory(node)) - ((char*) allocator->buffer)));
 
         fprintf(file, "\t}\n");
 
-        node = node_next(allocator, node);
+        node = fixed_buffer_node_next(allocator, node);
     }
 
     fprintf(file, "]\n");
@@ -297,7 +233,7 @@ void fixed_buffer_allocator_debug(fixed_buffer_allocator_t* allocator, FILE* fil
 
 size_t fixed_buffer_allocator_debug_memory(fixed_buffer_allocator_t* allocator, void* memory)
 {
-    node_t* node = node_find(allocator, memory);
+    fixed_buffer_node_t* node = fixed_buffer_node_find(allocator, memory);
 
     if (node == NULL)
     {
